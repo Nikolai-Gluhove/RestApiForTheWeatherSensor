f377206ca2b1c793ea0e6a60e390e4b3ee00e83a
Nikolai-Gluhove <nik.gluhov98@mail.ru>
2025-09-23 22:53:16 +0300

    создал внедрения мапера модели в объекте AppConfig

---
+++ FirstRestApp/src/main/java/ru/alishev/springcourse/FirstRestApp/util/AppConfig.java
@@ -0,0 +1,15 @@
+package ru.alishev.springcourse.FirstRestApp.util;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
+}
